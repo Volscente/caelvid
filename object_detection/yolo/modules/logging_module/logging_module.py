02226a62e00ba8091feb0bec7f1e5f0b2131f700
Simone Porreca <porrecasimone@gmail.com>
2022-10-23 21:50:31 +0200

    Update exceptions logging_module

--- object_detection/yolo/modules/logging_module/logging_module.py
+++ object_detection/yolo/modules/logging_module/logging_module.py
@@ -18,12 +18,18 @@ def get_logger(logger_name: str) -> logging.Logger:
         logging.Logger logger object
     """
 
-    # Read the log_configuration file
-    with open('./configuration/log_configuration.yaml', 'r') as file:
-        log_config = yaml.safe_load(file.read())
-        logging.config.dictConfig(log_config)
+    try:
 
-    # Retrieve the requested logger
-    logger = logging.getLogger(logger_name)
+        # Read the log_configuration file
+        with open('./configuration/log_configuration.yaml', 'r') as file:
+            log_config = yaml.safe_load(file.read())
+            logging.config.dictConfig(log_config)
+
+        # Retrieve the requested logger
+        logger = logging.getLogger(logger_name)
+
+    except FileNotFoundError:
+
+        raise FileNotFoundError
 
     return logger
--- object_detection/yolo/modules/object_detection/object_detection.py
+++ object_detection/yolo/modules/object_detection/object_detection.py
@@ -6,6 +6,7 @@ os.chdir(os.environ['YOLO_OBJECT_DETECTION_PATH'])
 
 # Import Package Libraries
 from modules.logging_module.logging_module import get_logger
+from modules.utils.utils import read_configuration
 
 
 class ObjectDetector:
@@ -18,8 +19,21 @@ class ObjectDetector:
 
     """
 
-    def __init__(self):
+    def __init__(self, configuration_file='config.yaml'):
+        """
+        Initialise a ObjectDetector object for perform object detection operations
+
+        Args:
+            configuration_file: String of configuration file name
+        """
 
         # Setup Logger
         self.logger = get_logger(__class__.__name__)
         self.logger.info('__init__ - Instancing the class')
+
+        self.logger.info('__init__ - Read configuration file')
+
+        # Read Configuration file
+        self.config = read_configuration(configuration_file)
+
+        #
