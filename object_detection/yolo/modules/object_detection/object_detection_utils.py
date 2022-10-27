1bed1b9a5607f8540d42e69362627f8e53b7ced2
Simone Porreca <porrecasimone@gmail.com>
2022-10-27 20:58:14 +0200

    Add read_blob_from_local_image

--- object_detection/yolo/modules/object_detection/object_detection_utils.py
+++ object_detection/yolo/modules/object_detection/object_detection_utils.py
@@ -1,1 +1,23 @@
 # Import Standard Libraries
+import os
+
+from typing import Tuple
+
+# Set root path
+os.chdir(os.environ['YOLO_OBJECT_DETECTION_PATH'])
+
+# Import Package Libraries
+from modules.logging_module.logging_module import get_logger
+
+
+def read_blob_from_local_image(image: str,
+                               size: Tuple[int, int],
+                               scale_factor: float,
+                               swap_rb: bool,
+                               crop: bool):
+
+    logger.info('read_blob_from_local_image - Start')
+
+def read_blob_from_rest_api_image():
+
+    pass
