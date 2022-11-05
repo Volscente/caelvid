09bf2aea1702976759fdf89d40fe0c6f551c5193
Simone Porreca <porrecasimone@gmail.com>
2022-11-05 09:11:01 +0100

    Add test_configuration fixture

--- object_detection/yolo/modules/pytest_test/test_utils_fixtures.py
+++ object_detection/yolo/modules/pytest_test/test_utils_fixtures.py
@@ -1,12 +1,27 @@
 # Import Libraries Modules
 import pytest
 import os
+import numpy as np
 
 # Set root path
 os.chdir(os.environ['YOLO_OBJECT_DETECTION_PATH'])
 
 # Import Package Libraries
 from modules.object_detection.object_detection import ObjectDetector
+from modules.object_detection.object_detection_utils import read_blob_from_local_image
+from modules.utils.utils import read_configuration
+
+
+@pytest.fixture
+def test_configuration() -> dict:
+    """
+    Fixture for a Dictionary configuration object
+
+    Returns:
+        Dictionary configuration
+    """
+
+    return read_configuration('config.yaml')
 
 
 @pytest.fixture
@@ -22,4 +37,15 @@ def test_object_detector() -> ObjectDetector:
 
 
 @pytest.fixture
-def test_blob()
+def test_blob(test_configuration) -> np.ndarray:
+    """
+    Fixture for a Blob image
+
+    Args:
+        test_configuration: Dictionary configuration object
+
+    Returns:
+        Numpy.ndarray Blob
+    """
+
+    pass
