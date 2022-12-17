1c9371580e1d52c993b507ec61e71111c3890320
Simone Porreca <porrecasimone@gmail.com>
2022-12-17 19:22:12 +0100

    Setup REST API Logger

--- object_detection/yolo/packages/api_service/rest_api.py
+++ object_detection/yolo/packages/api_service/rest_api.py
@@ -11,23 +11,17 @@ os.chdir(os.environ['YOLO_OBJECT_DETECTION_PATH'])
 from packages.logging_module.logging_module import get_logger
 from packages.object_detection.object_detection import ObjectDetector
 
+# Setup logger
+logger = get_logger(os.path.basename(__file__).split('.')[0])
+
 # Instance FastAPI object
 app = FastAPI()
 
-@app.post('/file')
-def upload_image(image_file: UploadFile = File(...)):
+
+@app.post('/detect_object/')
+def detect_object(image: UploadFile = File(..., description='Image file from which detect the object')):
 
     # Read the 'SpooledTemporaryFile' in image_file.file.read() as Numpy Array
-    numpy_array = np.frombuffer(image_file.file.read(), np.uint8)
+    image_numpy = np.frombuffer(image.file.read(), np.uint8)
 
-    # Read OpenCV Image
-    image = cv2.imdecode(numpy_array, cv2.IMREAD_UNCHANGED)
-
-    # Read blob from image
-    blob = cv2.dnn.blobFromImage(image=image,
-                                 size=(416, 416),
-                                 scalefactor=1/255.,
-                                 swapRB=True,
-                                 crop=False)
-
-    return {'Blob Shape': blob.shape}
+    return {'detected_object': None}
